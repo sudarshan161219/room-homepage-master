01c8b94715bfc82da607d81a15a1634862b23e0a
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-06-19 09:23:57 +0530

    update

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const slides = document.querySelectorAll('.slide');
-// const modalSlides = document.querySelectorAll('.modal-slide');
+
 
 const nextBtn = document.querySelectorAll('.next');
 const prevBtn = document.querySelectorAll('.previous');
 
-const img = document.querySelectorAll('.product-img')
+const img = document.querySelectorAll('.product-img');
+
+const heading = document.querySelector('.heading-one');
+const paragraph = document.querySelector('.paragraph-one');
+
+
+
 
 
 slides.forEach(function (slide, index) {
     slide.style.left = `${index * 100}%`;
+
+    
  });
  
-//  modalSlides.forEach(function (slide, index) {
-//     slide.style.left = `${index * 100}%`;
-//  });
+
 
 let counter = 0;
 nextBtn.forEach(function (next) {
    next.addEventListener("click", function () {
       counter++;
       carousel();
+      text ()
+    
+          
+ 
    });
 });
 
@@ -28,18 +38,53 @@ prevBtn.forEach(function (previous) {
    previous.addEventListener("click", function () {
       counter--;
       carousel();
+      text ()
    });
 });
+
+
 
 function carousel() {
     if (counter === slides.length) {
        counter = 0;
+      
     }
     if (counter < 0) {
        counter = slides.length - 1;
+      
     }
     slides.forEach(function (slide) {
        slide.style.transform = `translateX(-${counter * 100}%)`
+
+    
+
     });
+
+
+ 
+  
  }
 
+
+
+   console.log(slides)
+
+   function text (){
+      if(counter === (slides.length = 0) ){
+         heading.textContent = ' Discover innovative ways to decorate'
+         paragraph.textContent = 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
+        }
+
+      if(counter === (slides.length = 1) ){
+         heading.textContent = ' We are available all across the globe'
+         paragraph.textContent = `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
+         store locator. Any questions? Don't hesitate to contact us today.`
+        }
+      
+       if(counter === (slides.length = 2)){
+         heading.textContent = ' Manufactured with the best materials'
+         paragraph.textContent = `  Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+         to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+         experience in this industry, we understand what customers want for their home and office.`
+        }
+   }
